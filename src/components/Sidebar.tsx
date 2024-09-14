919866eb884fc115bb6a4ace792020f0c60b4694
Daniel Zhang <z331317@gmail.com>
2024-09-14 16:08:16 -0400

    Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,18 +13,24 @@ function Sidebar({handleClose, isOpen}: SidebarProps) {
   const [openProfile, setOpenProfile] = useState(false);
   const [openFeed, setOpenFeed] = useState(false);
   const [openMap, setOpenMap] = useState(false);
-  const [openFriends, setOpenFriends] = useState(false);
+  const [openFriends, setOpenFriends] = useState(false); 
+  return (  
 
-  if (!isOpen) return null;
+  <div
+  className={`sidebar flex flex-col bg-slate-300 w-24 h-screen z-10 items-center fixed ease-in-out duration-300 ${
+    isOpen ? "translate-x-0 " : "-translate-x-full"
+  }`}>  
 
-  return (
-    <div className="sidebar">
+      <div className = "h-8"></div>
+      <div onClick={handleClose} className = "cursor-pointer w-[15px] h-[15px] mt-[10px] mb-[20px]">
+        <img src="/close.png"></img>
+     </div> 
       <IconButton onClick={() => setOpenFeed(true)} name="Activity" icon="/activity.png" />
       <IconButton onClick={() => setOpenMap(true)} name="Map" icon="/pin.png" />
       <IconButton onClick={() => setOpenFriends(true)} name="Friends" icon="/friends.png" />
       <IconButton onClick={() => setOpenProfile(true)} name="Profile" icon="/user.png" />
-
-      <button onClick={handleClose}>Close</button>
+      <IconButton onClick={() => null} name="Settings" icon="/settings.png" />
+ 
 
       <ProfilePopup handleClose={() => setOpenProfile(false)} isOpen={openProfile} />
     </div>
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -8,7 +8,9 @@ function TestPage() {
 
   return (
     <div> 
-      <IconButton onClick={() => setOpenSidebar(true)} name="" icon="/menu.png" />
+        {!openSidebar && <div className="cursor-pointer left-0 ml-[30px] float-left absolute mt-[20px] h-[30px] w-[30px] inline-block z-3">
+            <IconButton onClick={() => setOpenSidebar(true)} name="" icon="/menu.png"/>
+        </div>}
       
       <Sidebar handleClose={() => setOpenSidebar(false)} isOpen={openSidebar}/>
     </div> 
